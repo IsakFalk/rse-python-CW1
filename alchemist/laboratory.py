516771a69a251e1ec9c9157263eae0357d4c230e
Isak Falk <isak.falk@live.se>
2018-11-07 16:05:24 +0000

    Define lab shelves attribute of Laboratory class

--- alchemist/laboratory.py
+++ alchemist/laboratory.py
@@ -2,5 +2,14 @@
 
 class Laboratory:
 
-    def __init__(self):
-        pass
+    def __init__(self, lower, upper):
+        """Initialise laboratory, with lab_shelves
+
+        lab_shelves is a dictionary of the lower and upper shelves
+        specified as lists of strings representing the substances of
+        this laboratory and its lower and upper shelves.
+
+        :param lower: list of strings specifying substances on lower shelf in lab
+        :param upper: list of strings specifying substances on upper shelf in lab
+        """
+        self.lab_shelves = dict(lower=lower, upper=upper)
