bc32a8c6b0c10b0b32b2f4afba00e9f72be52c9e
Isak Falk <isak.falk@live.se>
2018-11-07 16:10:53 +0000

    Implement can_react method of Laboratory class

--- alchemist/laboratory.py
+++ alchemist/laboratory.py
@@ -13,3 +13,17 @@ class Laboratory:
         :param upper: list of strings specifying substances on upper shelf in lab
         """
         self.lab_shelves = dict(lower=lower, upper=upper)
+
+    def can_react(self, substance1, substance2):
+        """Checks if two substances can react with eachother
+
+        Two substances, substance1 and substance2 are able to react if they
+        are such they are the anti-substances of each other. What this means is that
+        either the string of substance1 is the concatenation of 'anti' and the string of
+        subtance2 or the other way around.
+
+        :param substance1: string specifying a substance
+        :param substance2: string specifying another substance
+
+        :return: boolean specifying if the two substances can react or not"""
+        return (substance1 == "anti" + substance2) or (substance2 == "anti" + substance1)
