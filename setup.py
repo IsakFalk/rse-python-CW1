f0eac89633339ff80f30f85d412e821dd483706f
Isak Falk <isak.falk@live.se>
2018-12-01 22:33:49 +0000

    Update setup.py to specify pyyaml and pytest dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "Alchemist",
+    name = "alchemist",
     version = "0.1.0",
     packages = find_packages(exclude=['*test']),
-    install_requires = ['argparse'],
+    install_requires = ['argparse', 'pyyaml', 'pytest'],
     entry_points={
         'console_scripts': [
             'abracadabra = alchemist.command:process'
