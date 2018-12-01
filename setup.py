7c76f5a362e5ab0c14fa7fc2feea23b52155e808
Isak Falk <isak.falk@live.se>
2018-12-01 23:30:47 +0000

    Update setup.py with metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name = "alchemist",
     version = "0.1.0",
+    summary = "Library for simulating 2-shelf wizardry reactions.",
+    author = "Isak Falk",
+    author_email = "ucabitf@ucl.ac.uk",
+    licence = "MIT",
     packages = find_packages(exclude=['*test']),
     install_requires = ['argparse', 'pyyaml', 'pytest'],
     entry_points={
