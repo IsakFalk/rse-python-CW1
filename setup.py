604d95033ac3642dc23d36ecf1eeb71de711622a
Isak Falk <isak.falk@live.se>
2018-12-01 23:40:06 +0000

    Fix whitespace around assignment " = " -> "="

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "alchemist",
-    version = "0.1.0",
-    summary = "Library for simulating 2-shelf wizardry reactions.",
-    author = "Isak Falk",
-    author_email = "ucabitf@ucl.ac.uk",
-    licence = "MIT",
-    packages = find_packages(exclude=['*test']),
-    install_requires = ['argparse', 'pyyaml', 'pytest'],
+    name="alchemist",
+    version="0.1.0",
+    description="Library for simulating 2-shelf wizardry reactions.",
+    author="Isak Falk",
+    author_email="ucabitf@ucl.ac.uk",
+    licence="MIT",
+    packages=find_packages(exclude=['*test']),
+    install_requires=['argparse', 'pyyaml', 'pytest'],
     entry_points={
         'console_scripts': [
-            'abracadabra = alchemist.command:process'
+            'abracadabra=alchemist.command:process'
         ]
     }
 )
