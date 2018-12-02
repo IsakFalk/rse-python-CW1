a7ff026d49ba6e2193fba9e34b4a068d6bcf90ea
Isak Falk <isak.falk@live.se>
2018-12-02 00:23:36 +0000

    Add command line functionality

---
+++ alchemist/command.py
@@ -0,0 +1,21 @@
+import argparse
+import yaml
+
+from alchemist.laboratory import Laboratory
+
+def process():
+    parser = argparse.ArgumentParser(description="Run experiment on a wizardry 2-shelf laboratory")
+    parser.add_argument("shelves", default="laboratory.yaml", type=str, help="YAML file specifying the laboratory shelves")
+    parser.add_argument("--reactions", "-r", action="store_true", help="print the number of reactions that occurred during experiment")
+    args = parser.parse_args()
+
+    # Perform experiment using yaml file as input
+    with open(args.shelves, 'r') as f:
+        lab_shelves = yaml.load(f)
+    lab = Laboratory(lab_shelves['lower'], lab_shelves['upper'])
+    num_reactions, final_lower_shelf, final_upper_shelf = lab.run_full_experiment()
+
+    if args.reactions:
+        print(num_reactions)
+    else:
+        print("\nlower: {}\nupper: {}".format(final_lower_shelf, final_upper_shelf))
