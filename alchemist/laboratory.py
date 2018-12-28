cab99d707a5d19f245a41bd45558b5f8ac8bb757
Isak Falk <isak.falk@live.se>
2018-12-28 23:29:35 +0000

    Raise errors in Laboratory methods, clean up code

--- alchemist/laboratory.py
+++ alchemist/laboratory.py
@@ -5,28 +5,24 @@ import random
 class Laboratory:
     """Laboratory class for simulating wizardry on the standardised 2-shelf wizard lab"""
 
-    def __init__(self, lower, upper):
-        """Initialise laboratory, with lab_shelves
+    def __init__(self, lower=[], upper=[]):
+        """Initialise laboratory, with shelves
 
-        lab_shelves is a dictionary of the lower and upper shelves
+        shelves is a dictionary of the lower and upper shelves
         specified as lists of strings representing the substances of
         this laboratory and its lower and upper shelves.
 
         :param lower: list of strings specifying substances on lower shelf in lab
         :param upper: list of strings specifying substances on upper shelf in lab
         """
-        self.lab_shelves = dict(lower=lower, upper=upper)
-
-    def set_shelves(self, lower, upper):
-        """Restock the shelves with new substances
-
-        Passing in new lower and upper shelves, reset
-        the lab shelves
-
-        :param lower: list of strings specifying substances on lower shelf in lab
-        :param upper: list of strings specifying substances on upper shelf in lab
-        """
-        self.lab_shelves = dict(lower=lower, upper=upper)
+        # Check for faulty input
+        if not isinstance(lower, list) or not isinstance(upper, list):
+            raise TypeError(
+                "The lower and upper shelves need to be of type list.")
+        if (lower != [] and any([not isinstance(x, str) for x in lower])) or (upper != [] and any([not isinstance(x, str) for x in upper])):
+            raise TypeError(
+                "The substances of the shelves need to be strings.")
+        self.shelves = dict(lower=lower, upper=upper)
 
     def update_shelves(self, substance_lower, substance_upper_index):
         """Update the shelves by removing the substances reacting
@@ -39,11 +35,17 @@ class Laboratory:
 
         :return: tuple of two lists of the new updated lower and upper shelves in the lab
         """
-        index_lower = self.lab_shelves["lower"].index(substance_lower)
-        self.lab_shelves["lower"] = self.lab_shelves["lower"][:index_lower] + \
-            self.lab_shelves["lower"][index_lower+1:]
-        self.lab_shelves["upper"] = self.lab_shelves["upper"][:substance_upper_index] + \
-            self.lab_shelves["upper"][substance_upper_index+1:]
+        if not substance_lower in self.shelves["lower"]:
+            raise ValueError(
+                "The substance {} is not the lower shelf.".format(substance_lower))
+        if substance_upper_index < 0 or substance_upper_index >= len(self.shelves["upper"]):
+            raise IndexError(
+                "The index {} is out of range.".format(substance_upper_index))
+        index_lower = self.shelves["lower"].index(substance_lower)
+        self.shelves["lower"] = self.shelves["lower"][:index_lower] + \
+            self.shelves["lower"][index_lower+1:]
+        self.shelves["upper"] = self.shelves["upper"][:substance_upper_index] + \
+            self.shelves["upper"][substance_upper_index+1:]
 
     def can_react(self, substance1, substance2):
         """Checks if two substances can react with eachother
@@ -58,6 +60,8 @@ class Laboratory:
 
         :return: boolean specifying if the two substances can react or not
         """
+        if not isinstance(substance1, str) or not isinstance(substance2, str):
+            raise TypeError("The passed substances need to be strings.")
         return (substance1 == "anti" + substance2) or (substance2 == "anti" + substance1)
 
     def do_a_reaction(self):
@@ -78,9 +82,9 @@ class Laboratory:
         :return reaction_occurred: boolean specifying if a reaction occurred
         """
         reaction_occurred = False
-        for substance_lower in self.lab_shelves['lower']:
+        for substance_lower in self.shelves['lower']:
             possible_targets = [i for i, target in enumerate(
-                self.lab_shelves['upper']) if self.can_react(substance_lower, target)]
+                self.shelves['upper']) if self.can_react(substance_lower, target)]
             if not possible_targets:
                 continue
             else:
@@ -104,8 +108,9 @@ class Laboratory:
         terminates, printing the total number of reactions and return the final
         lower and upper shelves.
 
-        :return: (count, lower_shelf, upper_shelf) total number of reactions taken
-        place and the final shelves after running the experiment session
+        :return: (lower_shelf, upper_shelf, count) the final shelves after running the experiment session and total
+        number of reactions taken place
+
         """
         count = 0
         ended = False
@@ -116,4 +121,4 @@ class Laboratory:
             else:
                 # If no reaction occurred, the experiment is done
                 ended = True
-        return count, self.lab_shelves['lower'], self.lab_shelves['upper']
+        return self.shelves['lower'], self.shelves['upper'], count
